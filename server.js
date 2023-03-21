a5a392f85cdf1658955ac5b27608200da174b290
Jaskirat Singh <jmander135@gmail.com>
2023-03-21 16:39:47 -0400

    allowed data mutation

--- server.js
+++ server.js
@@ -23,6 +23,11 @@ async function start() {
 
   app.use(function (req, res, next) {
     res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+    res.setHeader(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content-Type, Accept"
+    );
+    res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE"); // Add PUT method
     next();
   });
 
